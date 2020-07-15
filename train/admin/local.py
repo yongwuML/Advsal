d6d5a1cae6bc01fed130070f485393ca9f9641c6
YongWu <42951782+yongwuSHU@users.noreply.github.com>
2020-07-15 12:35:00 +0800

    Update local.py

--- train/admin/local.py
+++ train/admin/local.py
@@ -1,5 +1,5 @@
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = '/home/yongwu/文档/YWuwork/Advsal/checkpoints'    # Base directory for saving network checkpoints.
-        self.resnext101_32_dir = '/home/yongwu/文档/YWuwork/R3Net/R3Net-master/resnext/resnext_101_32x4d.pth' #resneXt101
-        self.msra10k_dir = '/home/yongwu/文档/YWuwork/R3Net/MSRA10K'# Directory for MSRA10K dataset folder
+        self.workspace_dir = ''    # Base directory for saving network checkpoints.
+        self.resnext101_32_dir = '' #resneXt101
+        self.msra10k_dir = ''# Directory for MSRA10K dataset folder
