14bb87851c000171e9446c86c7c69fdcd0f44d2e
YongWu <42951782+yongwuSHU@users.noreply.github.com>
2020-07-15 12:36:39 +0800

    Update local.py

--- test/evaluation/local.py
+++ test/evaluation/local.py
@@ -5,13 +5,13 @@ def local_env_settings():
 
     # Set your local paths here.
 
-    settings.dutomron_path = '/home/yongwu/文档/YWuwork/R3Net/DUT-OMRON'
-    settings.dutste_path = '/home/yongwu/文档/YWuwork/R3Net/DUTS-TE'
-    settings.ecssd_path = '/home/yongwu/文档/YWuwork/R3Net/ECSSD'
-    settings.hkuis_path = '/home/yongwu/文档/YWuwork/R3Net/HKU-IS'
-    settings.network_path = '/home/yongwu/文档/YWuwork/Advsal/test/networks/'    # Where tracking networks are stored.
-    settings.pascal_path = '/home/yongwu/文档/YWuwork/R3Net/PASCAL850'
-    settings.results_path = '/home/yongwu/文档/YWuwork/Advsal/test/results/'    # Where to store tracking results
+    settings.dutomron_path = ''
+    settings.dutste_path = ''
+    settings.ecssd_path = ''
+    settings.hkuis_path = ''
+    settings.network_path = ''    # Where tracking networks are stored.
+    settings.pascal_path = ''
+    settings.results_path = ''    # Where to store tracking results
 
     return settings
 
